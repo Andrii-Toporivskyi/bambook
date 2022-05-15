d03a91c15dc42cc6954fca6490f4c834cab30046
Andrii <67384378+Andrii-Toporivskyi@users.noreply.github.com>
2022-05-15 11:44:23 +0300

    Add files via upload

---
+++ build/precache-manifest.a8e30a044201c09f0df23e18cc64d95b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "7195ea2fe77cb48481ec",
+    "url": "/static/css/main.659383ff.chunk.css"
+  },
+  {
+    "revision": "7195ea2fe77cb48481ec",
+    "url": "/static/js/main.7195ea2f.chunk.js"
+  },
+  {
+    "revision": "1412f651505e8c13f3ba",
+    "url": "/static/js/1.1412f651.chunk.js"
+  },
+  {
+    "revision": "bb0af5ca12c88720ccd8",
+    "url": "/static/css/2.a33bfe7a.chunk.css"
+  },
+  {
+    "revision": "bb0af5ca12c88720ccd8",
+    "url": "/static/js/2.bb0af5ca.chunk.js"
+  },
+  {
+    "revision": "86ef0b8aaa8d7eaf0c6f",
+    "url": "/static/js/runtime~main.86ef0b8a.js"
+  },
+  {
+    "revision": "57561ae8000c05c39925516a418cc27f",
+    "url": "/static/media/fon.57561ae8.jpg"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "f04ea0600ba12b556e1aec1c9b788376",
+    "url": "/index.html"
+  }
+];
